26af4a64bf32fc3f48e40249ecfb324f65a57423
tkf <tkf@users.noreply.github.com>
2020-10-04 06:12:29 +0000

    Deploying to gh-pages from  @ d8f383ae0319cf5942036e0d0256d86a6f770477 🚀

---
+++ previews/PR1/-test-/hist_collatz_stopping_time_with_executors.jl
@@ -0,0 +1,6 @@
+# This file was generated, do not modify it. # hide
+#hideall
+
+@test hist1 == hist2
+
+Base.Text("OK")
---
+++ previews/PR1/assets/tutorials/quick-introduction/code/os1.jl
@@ -0,0 +1,3 @@
+# This file was generated, do not modify it. # hide
+using OnlineStats: Mean
+e1 = ThreadsX.reduce(Mean(), 1:10)
