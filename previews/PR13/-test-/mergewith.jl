109615ae1176abf5d70422976886c2348fc85f62
tkf <tkf@users.noreply.github.com>
2020-10-05 07:40:04 +0000

    Deploying to gh-pages from  @ 4bfb8c51ee487f6fe4051abe73d6424b8c08f536 🚀

---
+++ previews/PR13/-test-/mergewith.jl
@@ -0,0 +1,6 @@
+# This file was generated, do not modify it. # hide
+#hideall
+
+@test f1 == f2
+
+Base.Text("OK")
---
+++ previews/PR13/assets/tutorials/quick-introduction/code/iter2.jl
@@ -0,0 +1,3 @@
+# This file was generated, do not modify it. # hide
+b4 = ThreadsX.collect(x + 1 for x in 1:3)
+@assert b1 == b4
