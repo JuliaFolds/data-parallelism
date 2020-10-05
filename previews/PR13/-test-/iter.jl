0eea4e0ca8578e50b538cd3f0904ab72b5e698d6
tkf <takafumi.a@gmail.com>
2020-10-05 07:43:09 +0000

    Deploying to gh-pages from  @ e9bd5aecb4a9f8ba304fc1830618f27eb1747a3a 🚀

--- previews/PR13/-test-/iter.jl
+++
@@ -1,9 +0,0 @@
-# This file was generated, do not modify it. # hide
-#hideall
-
-using Test
-    @testset begin
-        @test b1 == b2 == b3
-    end
-
-Base.Text("OK")
--- previews/PR13/assets/tutorials/quick-introduction/code/hist_collatz_stopping_time_with_executors.jl
+++
@@ -1,4 +0,0 @@
-# This file was generated, do not modify it. # hide
-hist1 = collatz_histogram(1:1_000_000, SequentialEx())
-hist2 = collatz_histogram(1:1_000_000, ThreadedEx())
-@assert hist1 == hist2
